8e8c3ab3dc959ac100c8fe45859c9eb070f77143
Carlos Britto <carlosbritto32@gmail.com>
2022-06-06 15:34:58 -0500

    Finishing the layout of the searchbar, now im going to add the logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,25 @@ import "../style/SearchBar.css";
 const SearchBar = () => {
   return (
     <React.Fragment>
-      <div className="list-searchbar"></div>
+      <form className="search__container">
+        <div className="search_location">
+          <label>Location</label>
+          <input type="text" placeholder="Where're you going?" />
+        </div>
+        <div className="search_arrivalDepartures">
+          <label>Arrival</label>
+          <input type="text" placeholder="Add dates" />
+        </div>
+        <div className="search_arrivalDepartures">
+          <label>Departure</label>
+          <input type="text" placeholder="Add dates" />
+        </div>
+        <div className="search_Guests">
+          <label>Guests</label>
+          <input type="text" placeholder="How Many?" />
+        </div>
+        <button>+</button>
+      </form>
     </React.Fragment>
   );
 };
